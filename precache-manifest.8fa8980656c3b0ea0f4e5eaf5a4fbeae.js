258d6fe5af30d494189bd723ad1db0ba9b668f7f
Choleslaw <41842273+kymiddleton@users.noreply.github.com>
2019-01-01 18:59:26 -0500

    Updates

---
+++ precache-manifest.8fa8980656c3b0ea0f4e5eaf5a4fbeae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f8fe840eb8fdebb5039",
+    "url": "/recipe/static/js/runtime~main.1f8fe840.js"
+  },
+  {
+    "revision": "1ce7fbae2b7af925967d",
+    "url": "/recipe/static/js/main.1ce7fbae.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/recipe/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "1ce7fbae2b7af925967d",
+    "url": "/recipe/static/css/main.181d24a3.chunk.css"
+  },
+  {
+    "revision": "69d667a401def2882cdad2e424fceb8b",
+    "url": "/recipe/index.html"
+  }
+];
