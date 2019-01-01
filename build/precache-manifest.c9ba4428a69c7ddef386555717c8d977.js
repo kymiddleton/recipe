84a6289dc52085d05bf05ede3c30b7441f2d8ffa
Choleslaw <41842273+kymiddleton@users.noreply.github.com>
2019-01-01 14:35:08 -0500

    following steps to deploy React app to Github Pages

---
+++ build/precache-manifest.c9ba4428a69c7ddef386555717c8d977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f8fe840eb8fdebb5039",
+    "url": "/recipe/static/js/runtime~main.1f8fe840.js"
+  },
+  {
+    "revision": "9137944b853bbffa3906",
+    "url": "/recipe/static/js/main.9137944b.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/recipe/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "9137944b853bbffa3906",
+    "url": "/recipe/static/css/main.181d24a3.chunk.css"
+  },
+  {
+    "revision": "415b1977726ef16ceddc4b126abfef4a",
+    "url": "/recipe/index.html"
+  }
+];
